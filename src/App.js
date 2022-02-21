fb87fc0d6eb7cbeafd7474c2aac36f781756d7f3
Lucas Vietos <lucasvieiramoura@gmail.com>
2022-02-20 21:46:15 -0300

    ReactJs Axios consult clima tempo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat,long)=>{
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather",{
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang:'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+    console.log(res.data);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+     getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  }, [])// Sempre que recarrega a tela chama o Effect de novo, passando o Array vazio no final ele chama uma única vez
+
+  if (location == false) { 
+    return (
+      <Fragment>
+        Você precisa habilitar a localização no browser o/
+      </Fragment>
+    )
+
+  }else if (weather == false) { //Como o processo para pegar os dados no endpoint da API é async quando não houver dados ainda exibe esta mensagem
+    return <Fragment>
+      Carregando o clima...
+    </Fragment>
+  
+  } else { // quando o endpoint da API retona as informações para o objeto weather carrega o Fragment abaixo
+    return (
+      <Fragment>
+        <h3>Clima nas duas Coordenadas ({weather['weather'][0]['description']}) 
+          <span><a  className={weather['weather'][0]['icon']}></a></span> 
+        </h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual:{weather['main']['temp']}</li>
+          <li>Temperatura máxima: {weather['main']['temp_max']}</li>
+          <li>Temperatura minima: {weather['main']['temp_min']}</li>
+          <li>Pressão: {weather['main']['pressure']} hpa</li>
+          <li>Umidade: {weather['main']['humidity']}</li>
+        </ul>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
